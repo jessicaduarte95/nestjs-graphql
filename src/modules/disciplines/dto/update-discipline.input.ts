35b4b17fd867845ed363d6f7a9d04bfc10e327e5
Jéssica Duarte <jessicaduarte.95@gmail.com>
2025-05-20 10:27:43 -0300

    🐛 Adjustment

--- src/modules/disciplines/dto/update-discipline.input.ts
+++ src/modules/disciplines/dto/update-discipline.input.ts
@@ -3,6 +3,6 @@ import { InputType, Field, PartialType, ID } from '@nestjs/graphql';
 
 @InputType()
 export class UpdateDisciplineInput extends PartialType(CreateDisciplineInput) {
-  @Field(() => ID)
+  @Field(() => ID, { nullable: true })
   id?: string;
 }
